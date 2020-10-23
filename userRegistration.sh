0b8b93a40b8fb8b8100eaa31388cbffe59717147
Vabs2 <vaibhavkadbhane2@gmail.com>
2020-10-23 12:59:10 +0530

    Checking for Email Validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,25 +1,46 @@
-#!/bin/bash
+#!/bin/bash -x
+shopt -s extglob
 
-checkDetails()
-{
 read -p "Enter First Name " firstname
 read -p "Enter Last Name " lastname
+read -p "Enter Email-id " email
 
-pattern="^[A-Z][a-z]{2,}$"
+namePattern="^[A-Z][a-z]{2,}$"
+emailPattern="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
 
-if [[ $firstname =~ $pattern ]]
-then
-   echo "Valid First Name"
-else
-   echo "Invalid First Name"
-fi
+checkFirstName()
+{
+	firstname=$1
 
-if [[ $lastname =~ $pattern ]]
-then
-   echo "Valid Last Name"
-else
-   echo "Invalid Last Name"
-fi
-
+	if [[ $firstname =~ $namePattern ]]
+	then
+		echo Valid First Name
+	else
+		echo Invalid First Name
+	fi
 }
-checkDetails
+checkLastName()
+{
+	lastname=$1
+
+   if [[ $lastname =~ $namePattern ]]
+   then
+      echo Valid Last Name
+   else
+      echo Invalid Last Name
+   fi
+}
+checkEmail()
+{
+	email=$1
+
+	if [[ $email =~ $emailPattern ]]
+	then
+   	echo Valid Email-Id
+	else
+   	echo Invalid Email-Id
+	fi
+}
+checkFirstName $firstname
+checkLastName $lastname
+checkEmail $email
